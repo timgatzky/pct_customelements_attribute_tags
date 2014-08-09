9596706eb91a9246a6c7131d6c73a48ba79248c6
Tim Gatzky <info@tim-gatzky.de>
2014-08-09 17:32:12 +0200

    add new be module icon

--- system/modules/pct_customelements_attributetags/config/config.php
+++ system/modules/pct_customelements_attributetags/config/config.php
@@ -26,7 +26,7 @@ array_insert($GLOBALS['BE_MOD']['content'], count($GLOBALS['BE_MOD']['content'])
 	'pct_customelements_tags' => array
 	(
 		'tables' 		=> array('tl_pct_customelement_tags'),
-		'icon'   		=> PCT_CUSTOMELEMENTS_TAGS_PATH.'/assets/img/icon.gif',
+		'icon'   		=> PCT_CUSTOMELEMENTS_TAGS_PATH.'/assets/img/tags_mod.png',
 	)
 ));
 
