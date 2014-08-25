645eec52198b6224713f459038c5ccf711222682
Tim Gatzky <info@tim-gatzky.de>
2014-08-25 13:00:15 +0200

    remove filter class

--- system/modules/pct_customelements_attribute_tags/config/autoload.php
+++ system/modules/pct_customelements_attribute_tags/config/autoload.php
@@ -8,7 +8,7 @@
  * @copyright	Tim Gatzky 2014, Premium Contao Webworks, Premium Contao Themes
  * @author		Tim Gatzky <info@tim-gatzky.de>
  * @package		pct_customelements
- * @subpackage	AttributeTags
+ * @subpackage	Tags
  * @link		http://contao.org
  */
 
@@ -31,6 +31,5 @@ ClassLoader::addClasses(array
 	'PCT\CustomElements\Attributes\Tags'								=> $path.'/PCT/CustomElements/Attributes/Tags/Tags.php',	
 	'PCT\CustomElements\Attributes\Tags\TableHelper'					=> $path.'/PCT/CustomElements/Attributes/Tags/TableHelper.php',	
 	'PCT\CustomElements\Attributes\Tags\TableCustomElementAttribute'	=> $path.'/PCT/CustomElements/Attributes/Tags/TableCustomElementAttribute.php',	
-	'PCT\CustomElements\Filters\Tags'									=> $path.'/PCT/CustomElements/Filters/FilterTags/Tags.php',	
 	'PCT\CustomElements\Backend\TableCustomElementTags'					=> $path.'/PCT/CustomElements/Backend/TableCustomElementTags.php',	
 ));
