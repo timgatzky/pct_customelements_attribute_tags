4ab9357b07286775dcfb7b8ca69e507828d32ac4
Tim Gatzky <info@tim-gatzky.de>
2014-12-23 11:12:31 +0100

    update to CE 1.2 format

--- system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
+++ system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
@@ -125,7 +125,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 	 * @return string
 	 * called renderCallback method
 	 */
-	public function renderCallback($strField,$varValue,$arrFieldDef,$objTemplate,$objAttribute)
+	public function renderCallback($strField,$varValue,$objTemplate,$objAttribute)
 	{
 		$varValue = deserialize($varValue);
 		
