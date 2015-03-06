02e4090ea1c5a09686bca99097b7b788a9b53b04
Tim Gatzky <info@tim-gatzky.de>
2015-03-06 11:25:53 +0100

    update

--- system/modules/pct_customelements_attribute_tags/dca/tl_pct_customelement_attribute.php
+++ system/modules/pct_customelements_attribute_tags/dca/tl_pct_customelement_attribute.php
@@ -31,10 +31,10 @@ $GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['config']['onload_callback'
 /**
  * Palettes
  */
-$type = 'tags';
+$strType = 'tags';
 $arrPalettes = $objDcaHelper->getPalettesAsArray('default');
 $arrPalettes['settings_legend'] = array('tag_custom','tag_roots','eval_multiple','eval_mandatory');
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['palettes'][$type] = $objDcaHelper->generatePalettes($arrPalettes);
+$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['palettes'][$strType] = $objDcaHelper->generatePalettes($arrPalettes);
 
 /**
  * Subpalettes
@@ -44,70 +44,69 @@ $objDcaHelper->addSubpalette('tag_custom',array('tag_table','tag_key','tag_value
 /**
  * Fields
  */
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_roots'] = array
+$objDcaHelper->addFields(array
 (
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_roots'],
-    'exclude'		=> true,
-    'inputType'		=> 'pct_tabletree',
-    'eval'			=> array('tl_class'=>'','tabletree'=>array('source'=>'tl_pct_customelement_tags'),'fieldType'=>'checkbox','multiple'=>true),
-    'sql'			=> "blob NULL"
-);
+	'tag_roots' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_roots'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'pct_tabletree',
+	    'eval'			=> array('tl_class'=>'','tabletree'=>array('source'=>'tl_pct_customelement_tags'),'fieldType'=>'checkbox','multiple'=>true),
+	    'sql'			=> "blob NULL"
+	),
+	'tag_custom' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_custom'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'checkbox',
+	    'eval'			=> array('tl_class'=>'','submitOnChange'=>true),
+	    'sql'			=> "char(1) NOT NULL default ''"
+	),
+	'tag_table' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_table'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'select',
+	    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getAllTables'),
+	    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'mandatory'=>true,'submitOnChange'=>true,'includeBlankOption'=>true,'decodeEntities'=>true),
+	    'sql'			=> "varchar(255) NOT NULL default ''"
+	),
+	'tag_key' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_key'],
+	    'exclude'		=> true,
+	    'default'		=> 'id',
+	    'inputType'		=> 'select',
+	    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
+	    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
+	    'sql'			=> "varchar(255) NOT NULL default ''"
+	),
+	'tag_value' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_value'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'select',
+	    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
+	    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
+	    'sql'			=> "varchar(255) NOT NULL default ''"
+	),
+	'tag_sorting' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_sorting'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'select',
+	    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
+	    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
+	    'sql'			=> "varchar(255) NOT NULL default ''"
+	),
+	'tag_where' => array
+	(
+	    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_where'],
+	    'exclude'		=> true,
+	    'inputType'		=> 'text',
+		'eval'			=> array('tl_class'=>'clr long','decodeEntities'=>true),
+	    'sql'			=> "varchar(255) NOT NULL default ''"
+	),
+));
 
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_custom'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_custom'],
-    'exclude'		=> true,
-    'inputType'		=> 'checkbox',
-    'eval'			=> array('tl_class'=>'','submitOnChange'=>true),
-    'sql'			=> "char(1) NOT NULL default ''"
-);
 
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_table'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_table'],
-    'exclude'		=> true,
-    'inputType'		=> 'select',
-    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getAllTables'),
-    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'mandatory'=>true,'submitOnChange'=>true,'includeBlankOption'=>true,'decodeEntities'=>true),
-    'sql'			=> "varchar(255) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_key'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_key'],
-    'exclude'		=> true,
-    'default'		=> 'id',
-    'inputType'		=> 'select',
-    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
-    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
-    'sql'			=> "varchar(255) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_value'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_value'],
-    'exclude'		=> true,
-    'inputType'		=> 'select',
-    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
-    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
-    'sql'			=> "varchar(255) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_sorting'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_sorting'],
-    'exclude'		=> true,
-    'inputType'		=> 'select',
-    'options_callback'	=> array('PCT\CustomElements\Attributes\Tags\TableHelper','getFields'),
-    'eval'			=> array('tl_class'=>'w50','chosen'=>true,'decodeEntities'=>true),
-    'sql'			=> "varchar(255) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_pct_customelement_attribute']['fields']['tag_where'] = array
-(
-    'label'  		=> &$GLOBALS['TL_LANG']['tl_pct_customelement_attribute']['tag_where'],
-    'exclude'		=> true,
-    'inputType'		=> 'text',
-	'eval'			=> array('tl_class'=>'clr long','decodeEntities'=>true),
-    'sql'			=> "varchar(255) NOT NULL default ''"
-);
