a74ce93f68d36f492aab531b23a079da46b43aad
Tim Gatzky <info@tim-gatzky.de>
2015-03-10 09:06:31 +0100

    update to version 1.4.3

--- system/modules/pct_customelements_attribute_tags/config/config.php
+++ system/modules/pct_customelements_attribute_tags/config/config.php
@@ -17,7 +17,7 @@
  * Constants
  */
 define(PCT_CUSTOMELEMENTS_TAGS_PATH, 'system/modules/pct_customelements_attribute_tags');
-define(PCT_CUSTOMELEMENTS_TAGS_VERSION, '1.4.2');
+define(PCT_CUSTOMELEMENTS_TAGS_VERSION, '1.4.3');
 
 /**
  * Back end modules
