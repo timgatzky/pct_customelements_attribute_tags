f54f0260020bd21f2ba60b58e9c2b9439bf403d4
Tim Gatzky <info@tim-gatzky.de>
2015-03-30 11:20:52 +0200

    pass the name of the roots field

--- system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
+++ system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
@@ -75,6 +75,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 		
 		// set root nodes
 		$arrReturn['tabletree']['roots'] = deserialize($this->get('tag_roots'));
+		$arrReturn['tabletree']['rootsField'] = 'tag_roots';
 		
 		// make field sortable
 		#$arrReturn['sortable'] = true;
