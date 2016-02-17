3930c90e506d1f91482d8cd3229df39a1d4b2692
Tim Gatzky <info@tim-gatzky.de>
2016-02-17 13:16:10 +0100

    added label

--- system/modules/pct_customelements_attribute_tags/languages/en/tl_pct_customelement_tags.php
+++ system/modules/pct_customelements_attribute_tags/languages/en/tl_pct_customelement_tags.php
@@ -44,3 +44,5 @@ $GLOBALS['TL_LANG']['tl_pct_customelement_tags']['delete'][0]			= 'Delete entry'
 $GLOBALS['TL_LANG']['tl_pct_customelement_tags']['delete'][1]			= 'Delete entry ID %s ?';
 $GLOBALS['TL_LANG']['tl_pct_customelement_tags']['copyChilds'][0]		= 'Copy tag ID %s including child elements';
 $GLOBALS['TL_LANG']['tl_pct_customelement_tags']['copyChilds'][1]		= 'Copy tag ID %s including child elements';
+$GLOBALS['TL_LANG']['tl_pct_customelement_tags']['show'][0]				= 'Informationen for Tag ID %s';
+$GLOBALS['TL_LANG']['tl_pct_customelement_tags']['show'][1]				= 'Show informationen for Tag ID %s';
