a87cad0de250be32a2f6eea7c1eb546f0cf7c6a4
Tim Gatzky <info@tim-gatzky.de>
2016-12-23 08:43:43 +0100

    update to version 1.7.3

--- system/modules/pct_customelements_attribute_tags/config/config.php
+++ system/modules/pct_customelements_attribute_tags/config/config.php
@@ -17,7 +17,7 @@
  * Constants
  */
 define(PCT_CUSTOMELEMENTS_TAGS_PATH, 'system/modules/pct_customelements_attribute_tags');
-define(PCT_CUSTOMELEMENTS_TAGS_VERSION, '1.7.2');
+define(PCT_CUSTOMELEMENTS_TAGS_VERSION, '1.7.3');
 
 $blnInstallTool = false;
 if(strlen(strpos(\Environment::getInstance()->scriptName, '/contao/install.php')) > 0 )
