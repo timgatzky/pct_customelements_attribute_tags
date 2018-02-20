1e1547540592cfc29177a95f1b33ab693b5f42ac
Tim Gatzky <info@tim-gatzky.de>
2018-02-20 11:50:51 +0100

    #75 PHP 7.2: replace count() with empty()

--- system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
+++ system/modules/pct_customelements_attribute_tags/PCT/CustomElements/Attributes/Tags/Tags.php
@@ -228,7 +228,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 				if(strlen($objResult->{$strTranslationField}) > 0)
 				{
 					$arrTranslations = deserialize($objResult->{$strTranslationField});
-					if(count($arrTranslations) > 0 && is_array($arrTranslations))
+					if(!empty($arrTranslations) && is_array($arrTranslations))
 					{
 						foreach($arrTranslations as $lang => $arrTranslation)
 						{
@@ -350,7 +350,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 				if(strlen($objResult->{$strTranslationField}) > 0)
 				{
 					$arrTranslations = deserialize($objResult->{$strTranslationField});
-					if(count($arrTranslations) > 0 && is_array($arrTranslations))
+					if(!empty($arrTranslations) && is_array($arrTranslations))
 					{
 						foreach($arrTranslations as $lang => $arrTranslation)
 						{
@@ -437,7 +437,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 				if(strlen($objResult->{$strTranslationField}) > 0)
 				{
 					$arrTranslations = deserialize($objResult->{$strTranslationField});
-					if(count($arrTranslations) > 0 && is_array($arrTranslations))
+					if(!empty($arrTranslations) && is_array($arrTranslations))
 					{
 						foreach($arrTranslations as $lang => $arrTranslation)
 						{
@@ -489,7 +489,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 	public function getBackendFilterOptions($arrData,$strField,$objAttribute,$objCC)
 	{
 		$arrOptions = $objAttribute->getSelectOptions();
-		if(count($arrOptions) < 1)
+		if(empty($arrOptions))
 		{
 			return array();
 		}
@@ -885,7 +885,7 @@ class Tags extends \PCT\CustomElements\Core\Attribute
 			if(strlen($objResult->{$strTranslationField}) > 0)
 			{
 				$arrTranslations = deserialize($objResult->{$strTranslationField});
-				if(count($arrTranslations) > 0 && is_array($arrTranslations) && array_key_exists($metaWizardKey, $arrTranslations))
+				if(!empty($arrTranslations) && is_array($arrTranslations) && array_key_exists($metaWizardKey, $arrTranslations))
 				{
 					foreach($arrTranslations as $lang => $arrTranslation)
 					{
